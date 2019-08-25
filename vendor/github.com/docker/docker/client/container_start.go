73c7d70d019f8730de7539e3c7e81cc871908d29
gaoweibupt <gaowei16@baidu.com>
2019-08-25 11:14:44 +0800

    add comments on docker run

--- vendor/github.com/docker/docker/client/container_start.go
+++ vendor/github.com/docker/docker/client/container_start.go
@@ -7,6 +7,7 @@ import (
 	"github.com/docker/docker/api/types"
 )
 
+
 // ContainerStart sends a request to the docker daemon to start a container.
 func (cli *Client) ContainerStart(ctx context.Context, containerID string, options types.ContainerStartOptions) error {
 	query := url.Values{}
@@ -17,6 +18,8 @@ func (cli *Client) ContainerStart(ctx context.Context, containerID string, optio
 		query.Set("checkpoint-dir", options.CheckpointDir)
 	}
 
+	// 这里实现的拉起容器 @gaoweibupt
+	//curl -XPOST --unix-socket /var/run/docker.sock "http://localhost/containers/5b7442223acc8d4a6fe0ead76f63754c4a955bec8417b3774e752c7f956fdb4b/start"
 	resp, err := cli.post(ctx, "/containers/"+containerID+"/start", query, nil, nil)
 	ensureReaderClosed(resp)
 	return err
